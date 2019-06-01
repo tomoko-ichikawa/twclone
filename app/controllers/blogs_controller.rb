7da3d8cae08eae2b5040cf52bb0a059652e74763
tomoko ichikawa <takator@yahoo.co.jp>
2019-06-01 12:24:16 +0900

    バリデーション

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,8 +8,12 @@ class BlogsController < ApplicationController
   end
   
   def create
-      Blog.create(blog_params)
-      redirect_to new_blog_path
+      @blog = Blog.new(blog_params)
+      if @blog.save
+          redirect_to new_blog_path, notice:"つぶやきを投稿しました"
+      else
+          render 'new'
+      end
   end
   
   def show
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,3 @@
 class Blog < ApplicationRecord
+    validates :content, length: { in: 1..140 }, presence: true
 end
