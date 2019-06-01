3b590ac2bc5357538268ec655193b4dccc5cc109
tomoko ichikawa <takator@yahoo.co.jp>
2019-06-01 13:13:57 +0900

    共通処理のメソッド化

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+    before_action :set_blog, only:[:show,:edit,:update]
+    
   def index
       @blogs = Blog.all
   end
@@ -17,15 +19,12 @@ class BlogsController < ApplicationController
   end
   
   def show
-      @blog = Blog.find(params[:id])
   end
   
   def edit
-      @blog = Blog.find(params[:id])
   end
   
   def update
-      @blog = Blog.find(params[:id])
       if @blog.update(blog_params)
           redirect_to blogs_path, notice: "つぶやきを編集しました"
       else
@@ -37,5 +36,9 @@ class BlogsController < ApplicationController
   
   def blog_params
       params.require(:blog).permit(:content)
+  end
+  
+  def set_blog
+      @blog = Blog.find(params[:id])
   end
 end
